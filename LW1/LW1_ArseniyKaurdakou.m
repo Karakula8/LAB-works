601f9ac6829b6cee7c78c0d49289736e6d43db64
Arseniy12 <arseniy.kaurdakov@gmail.com>
2026-09-14 09:48:41 +0300

    Update LW1_ArseniyKaurdakou.m

--- LW1/LW1_ArseniyKaurdakou.m
+++ LW1/LW1_ArseniyKaurdakou.m
@@ -10,3 +10,40 @@ plot(x, y, 'o-r', x, y/3, 'xb')
 title('Dvi funkcijos')
 xlabel('X-ai')
 ylabel('F_1 [-o-] | F_2 [-x-]')
+
+%% Complementary task
+
+N = 7;
+
+v = N + 1 : 0.5 : N + 4;
+
+A = N:N+8;
+A = reshape(A, 3, 3).';
+
+a = A(3,2);
+
+b = A(2:3,1:2);
+
+c = [A(1,1) A(1,3);
+     A(3,1) A(3,3)];
+
+v2 = v(1:3).';
+C = [A v2];
+
+disp('Vector v:')
+disp(v)
+
+disp('Matrix A:')
+disp(A)
+
+disp('a =')
+disp(a)
+
+disp('b =')
+disp(b)
+
+disp('c =')
+disp(c)
+
+disp('C =')
+disp(C)
